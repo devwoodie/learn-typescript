44385909b59b308143dce84ae867cfbb218159f9
devwoodie <ehddl453@naver.com>
2023-02-10 15:38:00 +0900

    feat : typescript 기본 타입

---
+++ class-note/1_type_basic.ts
@@ -0,0 +1,29 @@
+//js 문자열 선언
+// var str = "hello";
+
+//ts 문자열 선언
+let str: string = "hello";
+//ts 숫자 선언
+let num: number = 10;
+//ts 배열 선언
+let arr: Array<number> = [1,2,3];
+let heroes: Array<string> = ['Capt', 'Thor', 'Hulk'];
+let items : number[] = [1,2,3]
+
+//ts 튜플
+let address: [string, number] = ['gangnam', 10];
+
+//ts 객체
+let obj: object = {};
+// let person: object = {
+//     name: 'capt',
+//     age: 100
+// };
+
+let person : {name: string, age: number} = {
+    name: 'capt',
+    age: 100
+}
+
+// ts 진위값
+let show: boolean = true;
