467ebef47b17335d14ad17c3f40576d7936d40e9
devwoodie <ehddl453@naver.com>
2023-02-11 11:16:33 +0900

    feat : 함수의 옵셔널 파라미터

--- class-note/functions.ts
+++ class-note/functions.ts
@@ -3,3 +3,11 @@
 function sum(a: number, b:number):number {
     return a +b;
 };
+sum(10, 20);
+
+//함수의 옵셔널 파라미터(?)
+function log(a: string, b?: string, c?: string){
+
+}
+log('hello world');
+log('wow', 'hello');
